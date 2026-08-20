99525df37ad8e91f5af03533bcb661e378cd0841
UmairArshadCys <umairarshad1401@gmail.com>
2026-08-20 21:09:55 +0500

    Add files via upload

---
+++ CHAPTER 01-BASICS/02-Variables.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+
+int main() {
+    int number = 25;
+    char star = '*';
+    int age = 21;
+    float pi = 3.14;
+
+    int a = 10;
+    int A = 20;
+
+    int _age = 21;
+
+    int final_price = 100;
+
+    printf("Hello");
+    
+    return 0;
+}
