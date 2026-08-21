71f9a91c904ee4bb9a86a2fa9cf235d4cde69143
UmairArshadCys <umairarshad1401@gmail.com>
2026-08-21 18:16:38 +0500

    Add files via upload

---
+++ CHAPTER 01-BASICS/06-Format-Specifiers.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main() {
+    int age = 21;
+    printf("Age is %d \n", age);
+
+    float pi = 3.14;
+    printf("Vlaue of pi is %f \n", pi);
+
+    char star = '*';
+    printf("Star is %c \n", star);
+
+    return 0;
+}
