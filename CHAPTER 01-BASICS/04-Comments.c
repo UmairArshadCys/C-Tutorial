30ad1eb3e8811e9092a029e4751e51e5ea70242b
UmairArshadCys <umairarshad1401@gmail.com>
2026-08-21 18:00:47 +0500

    Add files via upload

---
+++ CHAPTER 01-BASICS/04-Comments.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+//this code displays how data types work in C
+
+/*
+     THis is a multi-line comment 
+     ,haojijojow
+     hajoilksa
+*/
+int main() {
+    int age = 21;
+    float pi = 3.14;
+    char hashtag = '#';
+    return 0;
+}
