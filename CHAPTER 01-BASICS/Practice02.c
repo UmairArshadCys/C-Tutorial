a48d73c34386b111186ee6351342ae45ade56596
UmairArshadCys <umairarshad1401@gmail.com>
2026-08-21 19:17:17 +0500

    Add files via upload

---
+++ CHAPTER 01-BASICS/Practice02.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+//area of square
+int main(){
+    int side;
+    printf("Enter the side ");
+    scanf("%d", &side);
+
+    printf("Area of cicle is : %d \n", side * side);
+    return 0;
+}
